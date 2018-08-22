3d23414653fa38ccc2c52c0720d982d6f3673ab7
Eda Hikari <m1m18306@st.oit.ac.jp>
2018-08-22 12:38:30 +0900

    first commit

---
+++ Assets/ScriptFolder/BallScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+    // speedを制御する
+    public float speed = 10;
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        // xとzにspeedを掛ける
+        rigidbody.AddForce(x * speed, 0, z * speed);
+    }
+}
---
+++ Assets/ScriptFolder/PieroScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieroScript : MonoBehaviour {
+
+    public Transform target;  // ターゲットへの参照
+    private Vector3 offset;  // 相対座標
+    private Animator anim;
+
+    void Start()
+    {
+        //自分自身とtargetとの相対距離を求める
+        offset = GetComponent<Transform>().position - target.position;
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // 自分自身の座標に、targetの座標に相対座標を足した値を設定する
+        GetComponent<Transform>().position = target.position + offset;
+
+        if (Input.GetKey("up"))
+        {
+            anim.SetBool("rolling", true);
+        }
+        else
+        {
+            anim.SetBool("rolling", false);
+        }
+
+        if (Input.GetKey("right"))
+        {
+            anim.SetBool("right", true);
+        }
+        else
+        {
+            anim.SetBool("right", false);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            anim.SetBool("left", true);
+        }
+        else
+        {
+            anim.SetBool("left", false);
+        }
+
+        if (Input.GetKey("down"))
+        {
+            anim.SetBool("back", true);
+        }
+        else
+        {
+            anim.SetBool("back", false);
+        }
+    }
+}
